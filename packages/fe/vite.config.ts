16b9076b5d3c7ba2e84f6667ae1a82d69ac44e79
itztla <hanumanman37@gmail.com>
2026-01-17 23:16:49 +0700

    rework fe

--- packages/fe/vite.config.ts
+++ packages/fe/vite.config.ts
@@ -1,13 +1,25 @@
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { URL, fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]]
-      }
-    })
-  ]
+    devtools(),
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    viteReact(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
